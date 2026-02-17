b4a4cbd8b46c6768bf82fe245580cee8a82c4376
omaroo123 <kkkb4780@leeds.ac.uk>
2026-02-17 16:59:42 +0000

    d

--- Application.kt
+++ Application.kt
@@ -1,4 +1,11 @@
-configureRouting()
-configureTemplates()
-configureDatabase()
-configureErrorHandling()
+import io.ktor.server.application.*
+import com.yourteam.library.configureRouting
+import com.yourteam.library.configureTemplates
+
+fun Application.module() {
+    // 1) Enable template rendering (.peb)
+    configureTemplates()
+
+    // 2) Add routes (/ , /login , /book/{id} etc.)
+    configureRouting()
+}
